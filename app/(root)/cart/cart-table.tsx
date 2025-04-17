a9214cdd6c68b038fd6cb9be90e2a9820b5a0bef
Yazukee <yasser.almaliki@live.se>
2025-04-17 07:30:01 +0200

    adjustmenst in validators

---
+++ app/(root)/cart/cart-table.tsx
@@ -0,0 +1,65 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { useToast } from "@/hooks/use-toast";
+import { Cart } from "@/types";
+import { useTransition } from "react";
+import { addItemToCart, removeItemFromCart } from "@/lib/actions/cart.actions";
+import { ArrowRight, Loader, Minus, Plus } from "lucide-react";
+import Link from "next/link";
+import Image from "next/image";
+import {
+  Table,
+  TableBody,
+  TableHead,
+  TableHeader,
+  TableRow,
+  TableCell,
+} from "@/components/ui/table";
+
+const CartTable = ({ cart }: { cart?: Cart }) => {
+  const router = useRouter();
+  const { toast } = useToast();
+  const [isPending, startTransition] = useTransition();
+  return (
+    <>
+      <h1 className="py-2 h2-bold">Shopping Cart</h1>
+      {!cart || cart.items.length === 0 ? (
+        <div>
+          Cart is empty. <Link href="/">Go Shopping</Link>
+        </div>
+      ) : (
+        <div className="grid md:grid-cols-4 md:gap-5">
+          <div className="overflow-x-auto md:col-span-3">
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Item</TableHead>
+                  <TableHead className="text-center">Quantity</TableHead>
+                  <TableHead className="text-center">Price</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {cart.items.map((item) => (
+                  <TableRow key={item.slug}>
+                    <TableCell>
+                      <Link href={`/product/${item.slug}`} className="flex">
+                        <Image
+                          src={item.images}
+                          alt={item.name}
+                          width={50}
+                          height={50}
+                        />
+                      </Link>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default CartTable;
--- lib/validators.ts
+++ lib/validators.ts
@@ -50,6 +50,7 @@ export const cartItemSchema = z.object({
   name: z.string().min(1, "Name is required"),
   slug: z.string().min(1, "Slug is required"),
   qty: z.number().int().nonnegative("Quantity must be a positive number"),
+  image: z.string().min(1, "Image is required"),
   price: currency,
 });
 
